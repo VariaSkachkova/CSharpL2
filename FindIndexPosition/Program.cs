205aaf37fe4a09fe9383fc8c7542bc6ac647010f
Varia S <especificita@gmail.com>
2022-06-29 12:23:59 +0300

    Find index of a given value in the array

---
+++ FindIndexPosition/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {11,25,31,4,54,66,72,48};
+int n = array.Length;
+int find = 66;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
+
