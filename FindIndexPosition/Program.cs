7d0ac725794a82c8191056d4366cc12075499ca1
Varia S <especificita@gmail.com>
2022-06-29 12:52:47 +0300

    Added code to fill array with random numbers and print index of a given value

--- FindIndexPosition/Program.cs
+++ FindIndexPosition/Program.cs
@@ -1,4 +1,4 @@
-﻿int [] array = {11,25,31,4,54,66,72,48};
+﻿int [] array = {11,25,31,4,66,66,72,48};
 int n = array.Length;
 int find = 66;
 int index = 0;
@@ -7,6 +7,7 @@ while (index < n)
     if(array[index] == find)
     {
         Console.WriteLine(index);
+        break;
     }
     index++;
 }
---
+++ Method+Array/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection) // void - метод, не возвращающий значения
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10); // положить в массив рандомное число из диапазона 1-10
+        index++;
+    }
+}
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++; 
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index< count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+
+        }
+        index++;
+    }
+    return position;
+}
+int [] array = new int [10]; //создание нового массива из 10 элементов, по умолчанию наполнен нулями
+
+FillArray (array);
+PrintArray (array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 45);
+Console.WriteLine(pos);
